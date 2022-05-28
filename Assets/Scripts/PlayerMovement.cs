d56d223053d4c5c3b201fd3ecf7952eb26ee3cd5
Kar Wah Chan <kw.chan@hotmail.co.uk>
2022-05-28 16:45:47 +0100

    Initial Commit First Upload

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float MovementSpeed = 2;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var movementside = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movementside, 0, 0) * Time.deltaTime * MovementSpeed;
+        var movementup = Input.GetAxis("Vertical");
+        transform.position += new Vector3(0, movementup, 0) * Time.deltaTime * MovementSpeed;
+    }
+}
